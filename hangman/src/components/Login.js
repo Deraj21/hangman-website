7c3b29faaeb89468e13d41ba621609187cdc6138
Jared Tanner <Deraj21@gmail.com>
2018-11-24 16:03:15 -0700

    if no user, get logged out

--- hangman/src/components/Login.js
+++ hangman/src/components/Login.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import Hangman from './Hangman';
 const HOSTED_SERVER="206.189.77.22";
 const LOCAL_SERVER="localhost";
-const IS_HOSTED=false;
 
 export default class Login extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      isHosted: false
+    };
+  }
+
+  componentDidMount(){
+    
+  }
   
   render() {
+
+    let { isHosted } = this.state;
 
     return (
       <div className="Login">
         <h1>Welcome to Hangman!</h1>
         <Hangman hangmanColor="black" backgroundColor="#BCFCFF" parts="11" />
-          <button><a href={`http://${IS_HOSTED ? HOSTED_SERVER : LOCAL_SERVER}:4000/login`}>Click Here to Login</a></button>
+          <button><a href={`http://${isHosted ? HOSTED_SERVER : LOCAL_SERVER}:4000/login`}>Click Here to Login</a></button>
       </div>
     );
   }
