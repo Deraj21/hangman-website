2b8d94c1cceb262897a99bcccb7c872875158385
Jared Tanner <Deraj21@gmail.com>
2019-04-23 19:47:21 -0700

    Delete env.js

--- src/env.js
+++
@@ -1,9 +0,0 @@
-const env = {
-  APP_BUILD: "production",
-  APP_LOCATION: "hosted",
-  PRODUCTION_LOCAL: "https://localhost:4000",
-  PRODUCTION_HOSTED: "",
-  BUILDING_LOCAL: "https://localhost:4000"
-};
-
-export default env
